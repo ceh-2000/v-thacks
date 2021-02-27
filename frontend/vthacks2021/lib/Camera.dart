e5876a410e614b0516eb7286d49f2cd43c451feb
Emilio Luz-Ricca <emiliolr@Emilios-MacBook-Air.local>
2021-02-27 13:31:22 -0500

    Added photo-taking functionality

--- frontend/vthacks2021/lib/Camera.dart
+++ frontend/vthacks2021/lib/Camera.dart
@@ -1,5 +1,38 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'Photo.dart';
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print(e);
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ML Vision',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: Camera(),
+    );
+  }
+}
 
 class Camera extends StatefulWidget {
   Camera({Key key}) : super(key: key);
@@ -9,17 +42,117 @@ class Camera extends StatefulWidget {
 }
 
 class _Camera extends State<Camera> {
-  @override
-  void initState() {}
+  CameraController _controller;
 
-  _Camera() {}
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(cameras[0], ResolutionPreset.medium);
+    _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<String> _takePicture() async {
+    if (!_controller.value.isInitialized) {
+      print("Controller is not initialized");
+      return null;
+    }
+
+    // Formatting Date and Time
+    String dateTime = DateFormat.yMMMd()
+        .addPattern('-')
+        .add_Hms()
+        .format(DateTime.now())
+        .toString();
+
+    String formattedDateTime = dateTime.replaceAll(' ', '');
+    print("Formatted: $formattedDateTime");
+
+    final Directory appDocDir = await getApplicationDocumentsDirectory();
+    final String visionDir = '${appDocDir.path}/Photos/Vision\ Images';
+    await Directory(visionDir).create(recursive: true);
+    final String imagePath = '$visionDir/image_$formattedDateTime.jpg';
+
+    if (_controller.value.isTakingPicture) {
+      print("Processing is progress ...");
+      return null;
+    }
+
+    try {
+      await _controller.takePicture(imagePath);
+    } on CameraException catch (e) {
+      print("Camera Exception: $e");
+      return null;
+    }
+
+    return imagePath;
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //       body: new Padding(
+  //           padding: const EdgeInsets.all(50.0), child: Text("Camera")
+  //       )
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: new Padding(
-            padding: const EdgeInsets.all(50.0), child: Text("Camera")
-        )
+      appBar: AppBar(
+        title: Text('Auto Grocer'),
+      ),
+      body: _controller.value.isInitialized
+          ? Stack(
+        children: <Widget>[
+          CameraPreview(_controller),
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: RaisedButton.icon(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color: Colors.teal[100],
+                icon: Icon(Icons.camera, color: Colors.teal,
+                  size: 60.0,),
+                label: Text("Click", style: TextStyle(fontSize: 60)),
+                onPressed: () async {
+                  await _takePicture().then((String path) {
+                    if (path != null) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Photo(),
+                        ),
+                      );
+                    }
+                  });
+                },
+              ),
+            ),
+          )
+        ],
+      )
+          : Container(
+        color: Colors.teal,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
     );
   }
+
 }
